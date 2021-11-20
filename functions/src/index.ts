d5d9de8c8c6283f42c9b1141a18003256e8e3e0c
taitranomi <taitran.3001@gmail.com>
2021-11-20 16:35:08 +0700

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,19 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
